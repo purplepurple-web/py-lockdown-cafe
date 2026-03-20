65bbe19b9fef75ac465c8d6fb1a9db9c3a1a3261
“Purple” <purplepurple773@gmail.comgit config --global user.email purplepurple773@gmail.com>
2026-03-20 18:27:46 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
+            # This catches both NotVaccinated and OutdatedVaccine errors
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
